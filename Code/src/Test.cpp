e9c95287d741d24c3d351e1ddccd1810c80ed084
cralbert79 <cralbert79@gmail.com>
2020-12-19 19:12:44 +0100

    Test de Comprobación de existencia del Espacio Natural para el Sendero

--- Code/src/Test.cpp
+++ Code/src/Test.cpp
@@ -79,6 +79,59 @@ void correctRutaReg()
 	 ASSERT(true); //BORRAR Y DESCOMENTAR LO DE ARRIBA CUANDO RUTA SE CORRIJA
 }
 
+void correctSenderotoEspacioNat(){
+	string nombre, espacioNat, estado;
+	int rutas;
+
+	fstream archivo;
+	archivo.open("test.txt", ios::out);
+
+	if(!archivo.is_open()) {
+		cout<<"Error al abrir el fichero de sendero.txt"<<endl;
+	}
+
+	nombre = "Sendero1";
+	espacionatural = "Cazorla";
+	estado = "Disponible";
+	rutas = 3;
+
+	ASSERT_EQUAL(existEspacioNat("test.txt", espacionatural), true);
+	archivo<<nombre<<"\t"<<espacionatural<<"\t"<<estado<<"\t"<<rutas<<endl;
+
+	nombre = "Sendero2";
+	espacionatural = "Cazorla";
+	estado = "Disponible";
+	rutas = 4;
+
+	ASSERT_EQUAL(existEspacioNat("test.txt", espacionatural), true);
+	archivo<<nombre<<"\t"<<espacionatural<<"\t"<<estado<<"\t"<<rutas<<endl;
+
+	nombre = "Sendero3";
+	espacionatural = "Cazorla";
+	estado = "Disponible";
+	rutas = 3;
+
+	ASSERT_EQUAL(existEspacioNat("test.txt", espacionatural), true);
+	archivo<<nombre<<"\t"<<espacionatural<<"\t"<<estado<<"\t"<<rutas<<endl;
+
+	nombre = "Sendero4";
+	espacionatural = "Cazorla";
+	estado = "Disponible";
+	rutas = 3;
+
+	ASSERT_EQUAL(existEspacioNat("test.txt", espacionatural), true);
+	archivo<<nombre<<"\t"<<espacionatural<<"\t"<<estado<<"\t"<<rutas<<endl;
+
+	nombre = "Sendero5";
+	espacionatural = "Grazalema";
+	estado = "Disponible";
+	rutas = 3;
+
+	ASSERT_EQUAL(existEspacioNat("test.txt", espacionatural), false);
+
+	archivo.close();
+}
+
 bool runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
 	//TODO add your test here
